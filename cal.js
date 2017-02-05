542c8c9c3f3161e03e49732f1a7f2aeaccc15d30
HaeyoonJo <jhys0226@gmail.com>
2017-02-05 23:47:33 +0900

    -a

--- cal.js
+++ cal.js
@@ -1,13 +1,26 @@
+/*
+	calculate
+*/
 console.log(process.argv);
 
 var num1 = parseInt(process.argv[2]);
-var num2 = process.argv[3];
-var num3 = parseInt(process.argv[4]);
+var op = process.argv[3];
+var num2 = parseInt(process.argv[4]);
 
-console.log(num1);
+console.log(num1, op, num2);
+/*
 console.log(num2);
 console.log(num3);
+*/
 
-if(num2 == '+'){console.log('result:', num1 + num3);} 
+if(op === '+'){
+  console.log('result: ' , num1 + num2);
+}else if(op === '-'){
+  console.log('result: ' , num1 - num2);
+}
+
+/*
+if(num2 == '+'){console.log('result:', num1 + num3);}
 if(num2 == '-'){console.log('result:', num1 - num3);}
 if(num2 == 'null'){console.log('result:', 'nothing');}
+*/
--- module.js
+++ module.js
@@ -1,12 +1,11 @@
+/*
+	build server side
+*/
 var http = require('http');
 
 var cnt = 0;
-
-var server = http.createServer(function(req, res) {
-	cnt = cnt + 1 ;
-	console.log(cnt);
-	
-	res.end('' + cnt);
+var server = http.createServer(function(req, res){
+	res.end('' + ++cnt);
 });
 
 server.listen(3000);
